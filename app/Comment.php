fbfdaf27a9b3266049c8319a6e107cbc3e4b780b
Alexander <acharlot91@gmail.com>
2017-10-03 19:12:58 +0200

    updating tests

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-  public $guarded = [];
+  protected $guarded = [];
+
+  protected $hidden = ['email', 'ip'];
+  protected $appends = ['email_md5', 'ip_md5'];
+
+  public function getEmailMd5Attribute () {
+    return md5($this->attributes['email']);
+  }
+  public function getIpMd5Attribute () {
+    return md5($this->attributes['ip']);
+  }
+
+  public static function allFor ($model, $model_id) {
+    $records = self::where(['commentable_id' => $model_id, 'commentable_type' => $model])->orderBy('created_at', 'ASC')->get();
+    $comments = [];
+    $by_id = [];
+    foreach($records as $record) {
+      if ($record->reply) {
+        $by_id[$record->reply]->attributes['replies'][] = $record;
+      } else {
+        $record->attributes['replies'] = [];
+        $by_id[$record->id] = $record;
+        $comments[] = $record;
+      }
+    }
+    return array_reverse($comments);
+  }
 }
--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    //
+  public function index () {
+    $comments = \App\Comment::allFor(\Illuminate\Support\Facades\Input::get('type'), \Illuminate\Support\Facades\Input::get('id'));
+    return \Illuminate\Support\Facades\Response::json($comments, 200, [], JSON_NUMERIC_CHECK);
+  }
 }
