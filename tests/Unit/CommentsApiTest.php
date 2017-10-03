bb9b0b1c17571df4ad33b426ef70f0e44f6f6903
Alexander <acharlot91@gmail.com>
2017-10-03 18:03:30 +0200

    Adding tests with faker for testing field

--- tests/Unit/CommentsApiTest.php
+++ tests/Unit/CommentsApiTest.php
@@ -10,7 +10,7 @@ class CommentsApiTest extends \Tests\TestCase {
   }
 
   public function testGetComments () {
-    $comment = Comment::create(['content' => 'Salut']);
+    $comment = factory(Comment::class)->create(['commentable_type' => 'Post', 'commentable_id' => 1]);
     $this->assertEquals(1, Comment::count());
   }
 }
